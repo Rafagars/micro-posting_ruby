4e2550c29145d8a6c6f24c8343b57b583a0d7ed0
rafagar96@gmail.com <rafagar96@gmail.com>
2020-05-11 18:37:10 -0400

    Changes in the style and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/new'
-  get 'static_pages/home'
+  get 'posts/index'
+  get 'posts/edit'
+  root 'static_pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
