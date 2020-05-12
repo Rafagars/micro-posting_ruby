397b2ff55d259d08e474109c14ec602e9f7beb47
rafagars <rafagar96@gmail.com>
2020-05-11 21:47:00 -0400

    Edit Post view

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_214123) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
