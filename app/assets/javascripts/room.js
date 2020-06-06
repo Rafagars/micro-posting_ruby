0264226ad44948f2c14c9002829c92ba0b9ac3e0
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-06 17:32:57 -0400

    Fixed Message no showing bug

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,5 +1,7 @@
-$(function(){
-    $("#new_room_message").on('ajax:success', function (a, b, c) {
-        $(this).find('input[type="text"]').val('');
+$(document).on('turbolinks:load', function () {
+    $(function(){
+        $("#new_room_message").on('ajax:success', function (a, b, c) {
+            $(this).find('input[type="text"]').val('');
+        });
     });
 });
