8c0b639392c7baf635a9231c90d1d1e6135616e4
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-06 17:25:01 -0400

    Chat finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
     name
   end
 
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
