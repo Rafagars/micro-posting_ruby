17536f60cc755712c8ed2fb886295abbf4ecb273
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-09 15:07:34 -0400

    Heroku commit

---
+++ config/environments/staging.rb
@@ -0,0 +1,8 @@
+# Just use the production settings
+require File.expand_path('../production.rb', __FILE__)
+
+Rails.application.configure do
+  # Here override any defaults
+  config.serve_static_files = true
+end
+
