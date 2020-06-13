4a9785421e56faf079e94fc58028ed737c5bf163
rafagars <rafagar96@gmail.com>
2020-06-13 05:30:46 -0400

    comment edit mobile bug

--- app/javascript/packs/controllers/edit_controller.js
+++ app/javascript/packs/controllers/edit_controller.js
@@ -3,8 +3,8 @@ import { Controller } from "stimulus"
 export default class extends Controller{
     static targets = ["body", "form"]
 
-    toggle() {
-        console.log(this.formTarget)
+    toggle(event) {
+    	event.preventDefault();
         this.bodyTarget.classList.toggle('d-none');
         this.formTarget.parentNode.classList.toggle('d-none');
     }
