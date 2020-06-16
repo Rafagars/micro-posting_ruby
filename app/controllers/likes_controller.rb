8c31932e9cc0a6ac5e0122c4816a20d6f361d479
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-16 10:39:14 -0400

    Fixed likes big

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,12 +5,12 @@ class LikesController < ApplicationController
 
   def create
     if post_already_liked(@post.id)
-      @like = Like.find_by(params[:user_id])
+      @like = Like.find_by(user_id: current_user.id, post_id: @post.id)
       @like.destroy
     else
       @post.likes.create(user_id: current_user.id)
     end
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
 
   private
