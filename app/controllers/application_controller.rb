cda02331ebb858e0d83c238ca7c13ab350756444
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-27 16:26:32 -0400

    Added some spanish Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-
-  get 'likes/create'
   root 'static_pages#home'
-  resources :posts do
-    resources :comments do
-      resources :hearts
+  scope "(:locale)", locale: /en|es/ do
+    get 'likes/create'
+    resources :posts do
+      resources :comments do
+        resources :hearts
+      end
+      resources :likes
     end
-    resources :likes
-  end
 
-  devise_for :users
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :room_messages
-  resources :rooms
+    devise_for :users
+    resources :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :room_messages
+    resources :rooms
+  end
 end
